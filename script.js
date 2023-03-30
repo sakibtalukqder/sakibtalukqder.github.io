73b3c139a56fd3df9653ecc2e57c64f5f6fb723b
Sakib Talukqder <126917076+sakibtalukqder@users.noreply.github.com>
2023-03-31 03:09:17 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,14 @@
-function Error(){
-  window.alert('Working on it, Try again letter. \nThank you.. :)');
-}
-
 //Sidebar Open & Close
 
 function w3_open() {
-  document.getElementById("mySidebar").style.display = "block";
-}
 
-function w3_close() {
-  document.getElementById("mySidebar").style.display = "none";
-}
+  var media_Nev = document.getElementById("def-nev");
 
-
-
-//--Dark-Theme
-
-
-function display() {
-  
-  let mode = document.getElementById("mode");
-  
-  document.body.classList.toggle("dark-thme");
-  if(document.body.classList.contains("dark-thme")){
-    mode.innerHTML='Lite';
+  if (media_Nev.style.display === "none") {
+    media_Nev.style.display = "flex";
   }
-  else{
-    mode.innerHTML='dark';
+  else {
+    media_Nev.style.display = "none";
   }
-  
+
 }
