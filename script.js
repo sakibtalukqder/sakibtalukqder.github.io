40d5b9ffeab87302f894f73eaf08606732000551
Sakib Talukqder <126917076+sakibtalukqder@users.noreply.github.com>
2023-03-31 04:15:22 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
+
 //Sidebar Open & Close
 
 function w3_open() {
 
-  var media_Nev = document.getElementById("def-nev");
+  let media_Nev = document.getElementById("def-nev");
 
-  if (media_Nev.style.display === "none") {
-    media_Nev.style.display = "flex";
-  }
-  else {
+  if(media_Nev.style.display === "flex"){
     media_Nev.style.display = "none";
   }
-
+  else{
+    media_Nev.style.display = "flex";
+  }
+  
 }
