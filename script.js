91c8c2b479dbd9030250a0e8a8d48d97c904d392
Sakib Talukqder <126917076+sakibtalukqder@users.noreply.github.com>
2023-04-02 01:11:39 +0600

    Update script.js

--- script.js
+++ script.js
@@ -11,5 +11,24 @@ function w3_open() {
   else{
     media_Nev.style.display = "flex";
   }
-  
+}
+
+
+//  __{{__back Scroll__}}__
+
+let ScrollButton = document.getElementById("ScrollButton");
+
+window.onscroll = function() { Scroll() };
+
+function Scroll() {
+  if (document.documentElement.scrollTop > 45) {
+    ScrollButton.style.display = "flex";
+  }
+  else {
+    ScrollButton.style.display = "none";
+  }
+}
+
+function Scroll_top() {
+  document.documentElement.scrollTop = 0;
 }
